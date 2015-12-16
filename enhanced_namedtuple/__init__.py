23e3d2a78a20a2a43da7e33fac44d16d0fece09e
Steve Zelaznik <zelaznik@yahoo.com>
2015-12-16 03:22:28 -0800

    Ready to push to GitHub.

--- enhanced_namedtuple/__init__.py
+++ enhanced_namedtuple/__init__.py
@@ -1,8 +1,17 @@
 from collections import namedtuple as orig_namedtuple
-from abc import ABCMeta, abstractmethod, abstractproperty
+from abc import ABCMeta
 __all__ = ['namedtuple']
 
 class namedtuple_meta(ABCMeta):
+    @classmethod
+    def instance(mcls):
+        try:
+            return mcls.__instance
+        except AttributeError:
+            cls = ABCMeta.__new__(mcls, 'namedtuple', (tuple,), {})
+            mcls.__instance = cls
+            return cls
+
     def __new__(mcls, name, bases, dct):
         _fields = dct.pop('_fields', None)
         __slots__ = dct.pop('__slots__', ())
@@ -21,14 +30,15 @@ class namedtuple_meta(ABCMeta):
         namedtuple.register(nt)
         return nt
 
-    def __call__(self, name, field_names = None, verbose=False, rename=False):
+    def __call__(cls, name, field_names = None, verbose=False, rename=False):
+        """%s""" % (orig_namedtuple.__doc__,)
         if field_names is None:
-            raise TypeError(("Cannot create instances of the abstract class namedtuple.\n"
-            "If you are using this method as a factory to create a namedtuple class,\n"
-            "you ned to include 'field_names' as an array or as a delimited string."))
+            raise TypeError(("Cannot create instances of the abstract class 'namedtuple'.\n"
+            "If you want to use this method as a factory to create a namedtuple class,\n"
+            "you need to include 'field_names' as an array or as a delimited string."))
 
         nt = orig_namedtuple(name, field_names, verbose, rename)
         namedtuple.register(nt)
         return nt
 
-namedtuple = ABCMeta.__new__(namedtuple_meta, 'namedtuple', (tuple,), {})
+namedtuple = namedtuple_meta.instance()
